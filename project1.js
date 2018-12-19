8b0c8f98dd43a72ac5f96d2e0e3038bb38f519e2
manzub <41767916+manzub@users.noreply.github.com>
2018-12-19 03:40:51 +0100

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,11 @@
+
+
+    document.querySelector("h1").addEventListener("click", function(){
+        document.body.style.color = randColor();
+        })
+function randColor(){
+    return '#' + (function co(lor){ return(lor +=
+    [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+[Math.floor(Math.random()*16)])
+&&(lor.length == 6)? lor : co(lor); })('');
+}
